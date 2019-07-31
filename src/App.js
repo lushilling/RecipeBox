6228033d0ca85a4d1b7bd6f8d7e8c6b41bf625c1
lushilling <lshilling@hotmail.co.uk>
2019-07-31 17:17:56 +0100

    should create a list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 import NavigationBar from './Components/NavigationBar';
+import RecipeList from './Components/RecipeList';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.onLoad();
+  }
+
+  onLoad = () => {
+    axios.get("url")
+      .then(response => {
+        this.setState({
+          data: response.data
+        })
+      })
+  }
+
   render() {
     return (
       <div >
         <NavigationBar />
+        <RecipeList />
       </div >
     )
   }
